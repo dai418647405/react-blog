c2817c682a37638ef95f2a6effe7da37885e26bf
daijing03 <daijing03@meituan.com>
2017-07-03 18:28:13 +0800

    1.修复父子组件传参问题 2.修改虎扑帖区版块tab间距 3.修改文章详情页字体大小

--- src/page/content/Article.js
+++ src/page/content/Article.js
@@ -9,28 +9,34 @@ class Article extends Component {
         this.state = {article : ''};
     }
 
-    componentWillReceiveProps() {
-        console.log('start componentWillReceiveProps');
-        this.loadBlogById(this.props.params.articleId);
+    //bug详解
+    // componentWillReceiveProps() {
+    //     console.log('start componentWillReceiveProps' + this.props.params.articleId);
+    //     this.loadBlogById(this.props.params.articleId);
+    // }
+
+    componentWillReceiveProps(nextProps) {
+        // console.log('start componentWillReceiveProps' + nextProps.params.articleId);
+        this.loadBlogById(nextProps.params.articleId);
     }
 
     loadBlogById(blogId) {
-        console.log('start loadBlogById blogId=' + blogId);
+        // console.log('start loadBlogById blogId=' + blogId);
         const url = '/api/blog/get/' + blogId;
         const params = {};
         Req.get(url, params ,res => {
-            console.log('loadBlogById result =' + res.toString());
+            // console.log('loadBlogById result =' + res.toString());
             if (res.code == 200) {
                 const blog = res.data;
                 this.setState({article : blog.htmlContent});
             }
         });
-        console.log('end loadBlogById blogId=' + blogId);
+        // console.log('end loadBlogById blogId=' + blogId);
     }
 
     render() {
         return (
-            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Content style={{ padding: '0 24px', minHeight: 280, fontSize: '14px' }}>
                 <div dangerouslySetInnerHTML={{__html: this.state.article}}></div>
             </Content>
         );
